ea867b61b6cc1e14183ef267da4317fea4fd119b
florin392 <iordacheflorin3@yahoo.com>
2025-10-29 16:35:30 +0100

    setup redux store with redux persist

---
+++ src/store/store.js
@@ -0,0 +1,27 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from '@reduxjs/toolkit';
+
+const rootReducer = combineReducers({});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+      },
+    }),
+  devTools: import.meta.env.MODE !== 'production',
+});
+
+export const persistor = persistStore(store);
